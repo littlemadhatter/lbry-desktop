05a6f53182dbc2fbe57e130b83fc4f24534d6ef9
littlemadhatter <31356843+littlemadhatter@users.noreply.github.com>
2020-06-11 23:10:53 -0700

    Update to add icon

--- ui/component/channelAbout/view.jsx
+++ ui/component/channelAbout/view.jsx
@@ -88,6 +88,20 @@ function ChannelAbout(props: Props) {
             />
             )
           </div>
+          
+          <label>Delete Channel</label>
+          <div class="media__info-text">
+            <button title="Abandon Claim" aria-label="Abandon Claim" class="button button--secondary" type="button">
+              <span class="button__content">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon--Trash">
+                  <g><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
+                    </path>
+                  </g>
+                </svg>
+              </span>
+            </button>
+          </div>
+          
         </Fragment>
       </section>
     </div>
